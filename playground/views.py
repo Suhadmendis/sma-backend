4a975ac70444c2b70c247f0db378844db3b3a39a
Akila Mendis <akila@stacktech.io>
2022-07-21 20:31:26 +0530

    updated

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .operation import Operation as op
+import json
+from django.core import serializers
 # Create your views here.
-def say_hello(request):
-    print("Hello")
-    return HttpResponse('Hello World')
+def verify(request):
+    return HttpResponse(op.verify_tweepy())
+
+
+def getUser(request):
+
+    verifiedObject = op.getObject()
+
+    user = op.getUser(verifiedObject)
+    response_data = json.dumps(user._json)
+    
+   
+    
+    
+    
+    # operation = Operation("fdsfs", "23")
+    
+    return HttpResponse((response_data), content_type="application/json")
