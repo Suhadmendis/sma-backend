f3f01d1f5f1599332e152b8e95fa0d5af5085b3a
Akila Mendis <akila@stacktech.io>
2022-07-22 23:21:06 +0530

    fixes

--- storefront/settings.py
+++ storefront/settings.py
@@ -1,9 +1,3 @@
-import os
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 """
 Django settings for storefront project.
 
@@ -16,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,7 +25,7 @@ SECRET_KEY = 'django-insecure-qr9n#r0+4=%j=!ja*z6awn)q-(eeb3b^)epr^mafz%=56hf=3j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
