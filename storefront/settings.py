643d88d5d7d820dc99b5a76f276e27de0c07bb82
Akila Mendis <akila@stacktech.io>
2022-07-22 23:27:11 +0530

    fixes

--- storefront/settings.py
+++ storefront/settings.py
@@ -1,3 +1,10 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 """
 Django settings for storefront project.
 
@@ -10,7 +17,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
