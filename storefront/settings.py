ed0819f8fd9a013d2e8d9add18ef8f3ad7fdca97
Akila Mendis <akila@stacktech.io>
2022-07-22 22:36:17 +0530

    fixes

--- storefront/settings.py
+++ storefront/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for storefront project.
 
@@ -116,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
