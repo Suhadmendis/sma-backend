ab3142225adf2b20e4f4c32baf966605626a6aa3
Akila Mendis <akila@stacktech.io>
2022-07-24 01:46:08 +0530

    methods are added

--- playground/urls.py
+++ playground/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
 from . import views
 
-urlpatterns =[
+urlpatterns = [
     path('verify/', views.verify),
-    path('user/', views.getUser)
+    path('user/', views.getUser),
+
+    path('operation1/', views.getOperation1),
+    path('operation2/', views.getOperation2),
+    path('operation3/', views.getOperation3),
+    path('operation4/', views.getOperation4),
+    path('operation5/', views.getOperation5),
+    path('operation6/', views.getOperation6),
+    path('operation7/', views.getOperation7),
+    path('operation8/', views.getOperation8),
+    path('operation9/', views.getOperation9),
+    path('operation10/', views.getOperation10),
+    path('operation11/', views.getOperation11),
+    path('operation12/', views.getOperation12),
+    path('operation13/', views.getOperation13),
 ]
--- playground/views.py
+++ playground/views.py
@@ -4,6 +4,8 @@ from .operation import Operation as op
 import json
 from django.core import serializers
 # Create your views here.
+
+
 def verify(request):
     return HttpResponse(op.verify_tweepy())
 
@@ -14,11 +16,57 @@ def getUser(request):
 
     user = op.getUser(verifiedObject)
     response_data = json.dumps(user._json)
-    
-   
-    
-    
-    
-    # operation = Operation("fdsfs", "23")
-    
+
     return HttpResponse((response_data), content_type="application/json")
+
+
+def operation1(request):
+    return HttpResponse(('operation1'), content_type="application/json")
+
+
+def operation2(request):
+    return HttpResponse(('operation2'), content_type="application/json")
+
+
+def operation3(request):
+    return HttpResponse(('operation3'), content_type="application/json")
+
+
+def operation4(request):
+    return HttpResponse(('operation4'), content_type="application/json")
+
+
+def operation5(request):
+    return HttpResponse(('operation5'), content_type="application/json")
+
+
+def operation6(request):
+    return HttpResponse(('operation6'), content_type="application/json")
+
+
+def operation7(request):
+    return HttpResponse(('operation7'), content_type="application/json")
+
+
+def operation8(request):
+    return HttpResponse(('operation8'), content_type="application/json")
+
+
+def operation9(request):
+    return HttpResponse(('operation9'), content_type="application/json")
+
+
+def operation10(request):
+    return HttpResponse(('operation10'), content_type="application/json")
+
+
+def operation11(request):
+    return HttpResponse(('operation11'), content_type="application/json")
+
+
+def operation12(request):
+    return HttpResponse(('operation12'), content_type="application/json")
+
+
+def operation13(request):
+    return HttpResponse(('operation13'), content_type="application/json")
